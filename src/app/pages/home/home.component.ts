b45a7e967be6cb1aad3cd22b4a389d0dcbf4a1b6
Alvaro Barbero <albaol60n@gmail.com>
2024-11-05 11:18:57 +0100

    Service API

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  api = inject(ApiService);
+  router = inject(Router);
 
+  $state: WritableSignal<any> = signal({
+    type: 'nationality',
+    loading: false,
+    error: false,
+    data: [],
+  });
+
+  @Input()
+  set type(type: string) {
+    //1 - Analizamos el valor
+    //2 - LLamar al servicio
+
+    //Hemso recibido un cambio en la ruta
+    this.$state.update((state) => ({ ...state, loading: true, type: type }));
+
+    let request: any;
+    switch (type) {
+      case 'category':
+        request = this.api.getCategories();
+        break;
+
+      default:
+        request = this.api.getNationalities();
+    }
+
+    request.subscribe(
+      (data: any) => {
+        this.$state.update((state) => ({
+          ...state,
+          loading: false,
+          error: false,
+          data: data.map((m: any) =>
+            type == 'category' ? { name: m.strCategory } : { name: m.strArea }
+          ),
+        }));
+        console.log(data);
+      },
+      (err: any) => {
+        console.log(err);
+        this.$state.update((state) => ({
+          ...state,
+          loading: false,
+          error: true,
+          data: [],
+        }));
+      }
+    );
+  }
+
+  listRecipe(ingredient: string) {
+    // Ir a la página /recipe/tipo/ingredient
+    this.router.navigate(['recipes', this.$state().type, ingredient]);
+  }
 }
